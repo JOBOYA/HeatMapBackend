671614a62f5baa187571c443da22f2eab03b27ee
joseph boyadjian <lena.boya26@gmail.com>
2024-05-11 12:13:04 +0200

    chore: Update CORS origin in app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://dreamy-kitten-353dc9.netlify.app'
 }));
 app.use(express.json());
 
